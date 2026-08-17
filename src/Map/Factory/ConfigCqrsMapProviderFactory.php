178934fecd539fbc73b8fb71c1f47d6a27313d8f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 10:57:20 +0200

    Use ContainerValue for CQRS map factory

--- src/Map/Factory/ConfigCqrsMapProviderFactory.php
+++ src/Map/Factory/ConfigCqrsMapProviderFactory.php
@@ -4,26 +4,14 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Map\Factory;
 
-use Componenta\Config\Config;
-use Componenta\CQRS\ConfigKey;
+use Componenta\Config\ContainerValue;
 use Componenta\CQRS\Map\ConfigCqrsMapProvider;
 use Componenta\CQRS\Map\CqrsMapProviderInterface;
-use Psr\Container\ContainerInterface;
 
 final class ConfigCqrsMapProviderFactory
 {
-    public function __invoke(ContainerInterface $container): CqrsMapProviderInterface
+    public function __invoke(ContainerValue $container): CqrsMapProviderInterface
     {
-        $config = $container->get(ConfigKey::CONFIG);
-
-        if (!$config instanceof Config) {
-            throw new \LogicException(sprintf(
-                'Container entry "%s" must be a %s instance.',
-                ConfigKey::CONFIG,
-                Config::class,
-            ));
-        }
-
-        return new ConfigCqrsMapProvider($config);
+        return new ConfigCqrsMapProvider($container->config);
     }
 }
