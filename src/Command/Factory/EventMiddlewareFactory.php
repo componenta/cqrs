57f8b4ec93aab3b67b4c7011d5f9a2bfddeef4b4
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 11:07:12 +0200

    Create event middleware from ContainerValue

--- src/Command/Factory/EventMiddlewareFactory.php
+++ src/Command/Factory/EventMiddlewareFactory.php
@@ -4,43 +4,19 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Command\Factory;
 
-use Componenta\Config\Config;
+use Componenta\Config\ContainerValue;
 use Componenta\CQRS\Command\Locator\CommandListenersLocatorInterface;
 use Componenta\CQRS\Command\Middleware\EventMiddleware;
-use Componenta\CQRS\ConfigKey;
-use LogicException;
-use Psr\Container\ContainerInterface;
 
 final class EventMiddlewareFactory
 {
-    public function __invoke(ContainerInterface $container): EventMiddleware
+    public function __invoke(ContainerValue $container): EventMiddleware
     {
-        $config = $container->get(ConfigKey::CONFIG);
-
-        if (!$config instanceof Config) {
-            throw new LogicException(sprintf(
-                'Container entry "%s" must be a %s instance.',
-                ConfigKey::CONFIG,
-                Config::class,
-            ));
-        }
-
-        $locator = $container->get(CommandListenersLocatorInterface::class);
-
-        if (!$locator instanceof CommandListenersLocatorInterface) {
-            throw new LogicException(sprintf(
-                'Container entry "%s" must implement %s.',
-                CommandListenersLocatorInterface::class,
-                CommandListenersLocatorInterface::class,
-            ));
-        }
-
         return new EventMiddleware(
-            $locator,
-            suppressExceptions: $config->environment?->string(
-                'APP_ENV',
-                'development',
-            ) === 'production',
+            $container->get(
+                CommandListenersLocatorInterface::class,
+                CommandListenersLocatorInterface::class,
+            ),
         );
     }
 }
