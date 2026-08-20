d925d8f1ae81a4ab4b63b76ce12c91fc016a2d32
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 00:11:46 +0200

    Align operation factory with createdAt semantics

--- src/Command/OperationFactoryInterface.php
+++ src/Command/OperationFactoryInterface.php
@@ -4,57 +4,16 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Command;
 
-/**
- * Factory interface for creating operation instances.
- *
- * CommandBus uses this contract to create the operation passed through its
- * middleware pipeline.
- *
- * @example
- * ```php
- * // Custom factory with tracing
- * class TracedOperationFactory implements OperationFactoryInterface
- * {
- *     public function __construct(
- *         private string $serviceName,
- *     ) {}
- *
- *     public function create(object $command, array $attributes = []): OperationInterface
- *     {
- *         $operation = Operation::create($command, $attributes);
- *         $_SERVER['HTTP_X_TRACE_ID'] = $operation->id->toString();
- *         return $operation;
- *     }
- * }
- * ```
- *
- * @example
- * ```php
- * // Testing factory with predictable UUIDs
- * class TestOperationFactory implements OperationFactoryInterface
- * {
- *     private int $counter = 0;
- *
- *     public function create(object $command, array $attributes = []): OperationInterface
- *     {
- *         $this->counter++;
- *         return new Operation(
- *             id: Uuid::fromString(sprintf('00000000-0000-7000-8000-%012d', $this->counter)),
- *             command: $command,
- *             startedAt: new DateTimeImmutable('2024-01-01 00:00:00'),
- *             attributes: $attributes,
- *         );
- *     }
- * }
- * ```
- */
+/** Factory for creating command operation instances. */
 interface OperationFactoryInterface
 {
     /**
-     * Creates a new operation instance.
+     * Creates a new operation for one dispatch.
+     *
+     * Implementations that construct Operation directly should set createdAt to
+     * the operation creation time; execution start is not part of this contract.
      *
      * @param array<string, mixed> $attributes
-     * @return OperationInterface New operation ready for use
      */
     public function create(object $command, array $attributes = []): OperationInterface;
 }
