413be360c1ad1c7feace02c11534b1f91bca1604
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-20 21:47:00 +0200

    Enable strict types in command resolver contract

--- src/Command/Resolver/CommandNameResolverInterface.php
+++ src/Command/Resolver/CommandNameResolverInterface.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Componenta\CQRS\Command\Resolver;
 
 interface CommandNameResolverInterface
 {
-    public function supports(object $command): bool ;
+    public function supports(object $command): bool;
 
     public function resolve(object $command): string;
 }
