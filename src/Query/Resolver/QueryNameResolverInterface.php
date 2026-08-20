7e77730e145bb74cf4d50f538bbb7394556cb981
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-20 21:47:12 +0200

    Enable strict types in query resolver contract

--- src/Query/Resolver/QueryNameResolverInterface.php
+++ src/Query/Resolver/QueryNameResolverInterface.php
@@ -1,16 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Componenta\CQRS\Query\Resolver;
 
 interface QueryNameResolverInterface
 {
-    /**
-     * Can this resolver handle the given query?
-     */
     public function supports(object $query): bool;
 
-    /**
-     * Resolve the query name (used for permissions, logging, handler mapping).
-     */
     public function resolve(object $query): string;
 }
