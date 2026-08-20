ace549d24c819718c80ba4cadd7208ffe815e560
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-20 23:37:52 +0200

    Enforce string query context attribute names

--- src/Query/Context/Context.php
+++ src/Query/Context/Context.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Query\Context;
 
+use InvalidArgumentException;
+
 /**
  * Default immutable {@see ContextInterface} - stores attributes in an array.
  *
@@ -13,7 +15,10 @@ namespace Componenta\CQRS\Query\Context;
 final readonly class Context implements ContextInterface
 {
     /** @param array<string, mixed> $attributes */
-    public function __construct(public array $attributes = []) {}
+    public function __construct(public array $attributes = [])
+    {
+        self::assertAttributes($attributes);
+    }
 
     public function getAttribute(string $name, mixed $default = null): mixed
     {
@@ -29,19 +34,53 @@ final readonly class Context implements ContextInterface
 
     public function withAttribute(string $name, mixed $value): static
     {
+        self::assertAttributeName($name);
+
         return new self([...$this->attributes, $name => $value]);
     }
 
     public function withAttributes(array $attributes): static
     {
+        self::assertAttributes($attributes);
+
         return new self([...$this->attributes, ...$attributes]);
     }
 
     public function withoutAttribute(string $name): static
     {
+        self::assertAttributeName($name);
+
         $attributes = $this->attributes;
         unset($attributes[$name]);
 
         return new self($attributes);
+    }
+
+    /** @param array<array-key, mixed> $attributes */
+    private static function assertAttributes(array $attributes): void
+    {
+        foreach ($attributes as $name => $_) {
+            if (!is_string($name) || trim($name) === '') {
+                throw new InvalidArgumentException(
+                    'Query context attribute names must be non-empty strings.',
+                );
+            }
+        }
+    }
+
+    private static function assertAttributeName(string $name): void
+    {
+        if (trim($name) === '') {
+            throw new InvalidArgumentException(
+                'Query context attribute name must be a non-empty string.',
+            );
+        }
+
+        if (!is_string(array_key_first([$name => true]))) {
+            throw new InvalidArgumentException(sprintf(
+                'Query context attribute name "%s" is converted by PHP to an integer array key.',
+                $name,
+            ));
+        }
     }
 }
