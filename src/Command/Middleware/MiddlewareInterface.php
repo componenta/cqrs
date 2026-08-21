eafbb99a472859d4ffc41429ee90e1f91b6e6643
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 02:42:56 +0200

    Normalize command middleware contract

--- src/Command/Middleware/MiddlewareInterface.php
+++ src/Command/Middleware/MiddlewareInterface.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Componenta\CQRS\Command\Middleware;
 
 use Componenta\CQRS\Command\OperationInterface;
 
 interface MiddlewareInterface
 {
-    public function execute(OperationInterface $operation, OperationHandlerInterface $handler): OperationInterface ;
+    public function execute(
+        OperationInterface $operation,
+        OperationHandlerInterface $handler,
+    ): OperationInterface;
 }
