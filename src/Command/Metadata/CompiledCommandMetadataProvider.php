d2ab45163f94ea016ef360e496cfe986e493201e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 11:31:35 +0200

    Avoid repeated metadata declaration reflection

--- src/Command/Metadata/CompiledCommandMetadataProvider.php
+++ src/Command/Metadata/CompiledCommandMetadataProvider.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Command\Metadata;
 
-use Attribute;
 use Componenta\CQRS\Map\CqrsMapProviderInterface;
 use Componenta\CQRS\Map\Exception\InvalidCqrsMapException;
-use ReflectionClass;
 use Throwable;
 
 /**
@@ -20,9 +18,12 @@ use Throwable;
  */
 final readonly class CompiledCommandMetadataProvider implements CommandMetadataProviderInterface
 {
+    private CommandMetadataAttributeValidator $attributeValidator;
+
     public function __construct(
         private CqrsMapProviderInterface $mapProvider,
     ) {
+        $this->attributeValidator = new CommandMetadataAttributeValidator();
     }
 
     /**
@@ -42,7 +43,7 @@ final readonly class CompiledCommandMetadataProvider implements CommandMetadataP
             return null;
         }
 
-        return self::materialize(
+        return $this->materialize(
             $attribute,
             $descriptor->arguments,
         );
@@ -61,9 +62,9 @@ final readonly class CompiledCommandMetadataProvider implements CommandMetadataP
      * @param array<int|string, mixed> $arguments
      * @return T
      */
-    private static function materialize(string $attribute, array $arguments): object
+    private function materialize(string $attribute, array $arguments): object
     {
-        self::assertAttributeClass($attribute);
+        $this->attributeValidator->validate($attribute);
 
         try {
             return new $attribute(...$arguments);
@@ -73,34 +74,6 @@ final readonly class CompiledCommandMetadataProvider implements CommandMetadataP
                 $attribute,
                 $exception->getMessage(),
             ), previous: $exception);
-        }
-    }
-
-    /** @param class-string $attribute */
-    private static function assertAttributeClass(string $attribute): void
-    {
-        if (!class_exists($attribute)) {
-            throw new InvalidCqrsMapException(sprintf(
-                'Command metadata attribute class "%s" does not exist.',
-                $attribute,
-            ));
-        }
-
-        $reflection = new ReflectionClass($attribute);
-        $declarations = $reflection->getAttributes(Attribute::class);
-
-        if ($declarations === []) {
-            throw new InvalidCqrsMapException(sprintf(
-                'Command metadata class "%s" is not declared with #[Attribute].',
-                $attribute,
-            ));
-        }
-
-        if (($declarations[0]->newInstance()->flags & Attribute::TARGET_CLASS) === 0) {
-            throw new InvalidCqrsMapException(sprintf(
-                'Command metadata attribute "%s" must allow class targets.',
-                $attribute,
-            ));
         }
     }
 
