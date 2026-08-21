fc4bbac0191111cd229c894a1e78ce3455989222
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 12:24:59 +0200

    Avoid repeated metadata declaration reflection

--- src/Command/Metadata/CompiledCommandMetadataProvider.php
+++ src/Command/Metadata/CompiledCommandMetadataProvider.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Command\Metadata;
 
-use Attribute;
 use Componenta\CQRS\Map\CqrsMapProviderInterface;
 use Componenta\CQRS\Map\Exception\InvalidCqrsMapException;
-use ReflectionClass;
 use Throwable;
 
 /**
@@ -20,9 +18,12 @@ use Throwable;
  */
 final readonly class CompiledCommandMetadataProvider implements CommandMetadataProviderInterface
 {
+    private CommandMetadataAttributeValidator $attributeValidator;
+
     public function __construct(
         private CqrsMapProviderInterface $mapProvider,
     ) {
+        $this->attributeValidator = new CommandMetadataAttributeValidator();
     }
 
     /**
@@ -42,31 +43,10 @@ final readonly class CompiledCommandMetadataProvider implements CommandMetadataP
             return null;
         }
 
-        return self::materialize(
-            $attribute,
-            $descriptor->arguments,
-        );
-    }
-
-    public function isKnown(object|string $command): bool
-    {
-        return $this->mapProvider->map()->isKnownCommand(
-            self::className($command),
-        );
-    }
-
-    /**
-     * @template T of object
-     * @param class-string<T> $attribute
-     * @param array<int|string, mixed> $arguments
-     * @return T
-     */
-    private static function materialize(string $attribute, array $arguments): object
-    {
-        self::assertAttributeClass($attribute);
+        $this->attributeValidator->validate($attribute);
 
         try {
-            return new $attribute(...$arguments);
+            return new $attribute(...$descriptor->arguments);
         } catch (Throwable $exception) {
             throw new InvalidCqrsMapException(sprintf(
                 'Cannot materialize command metadata attribute "%s": %s',
@@ -76,32 +56,11 @@ final readonly class CompiledCommandMetadataProvider implements CommandMetadataP
         }
     }
 
-    /** @param class-string $attribute */
-    private static function assertAttributeClass(string $attribute): void
+    public function isKnown(object|string $command): bool
     {
-        if (!class_exists($attribute)) {
-            throw new InvalidCqrsMapException(sprintf(
-                'Command metadata attribute class "%s" does not exist.',
-                $attribute,
-            ));
-        }
-
-        $reflection = new ReflectionClass($attribute);
-        $declarations = $reflection->getAttributes(Attribute::class);
-
-        if ($declarations === []) {
-            throw new InvalidCqrsMapException(sprintf(
-                'Command metadata class "%s" is not declared with #[Attribute].',
-                $attribute,
-            ));
-        }
-
-        if (($declarations[0]->newInstance()->flags & Attribute::TARGET_CLASS) === 0) {
-            throw new InvalidCqrsMapException(sprintf(
-                'Command metadata attribute "%s" must allow class targets.',
-                $attribute,
-            ));
-        }
+        return $this->mapProvider->map()->isKnownCommand(
+            self::className($command),
+        );
     }
 
     private static function className(object|string $command): string
