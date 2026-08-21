c891255e05294202e1f77d85e6141d0a9ff93e1a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-21 02:44:05 +0200

    Enable strict types in query support contract

--- src/Query/Locator/QuerySupportAwareInterface.php
+++ src/Query/Locator/QuerySupportAwareInterface.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Componenta\CQRS\Query\Locator;
 
 interface QuerySupportAwareInterface
 {
-    /**
-     * Check if handler exists for query.
-     */
+    /** Check whether this locator can resolve the query. */
     public function supports(object $query): bool;
 }
